20d23fe3fa0997a0f4b15b5d9fb016b02ea48531
anhnguyen6535 <anhnguyen6535@gmail.com>
2025-01-03 13:23:09 -0700

    add color condition manager

---
+++ Assets/Scripts/ConditionManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ConditionManager : MonoBehaviour
+{
+    [SerializeField] List<Material> listMaterials = new List<Material>();
+    [SerializeField] int colorIndex;
+    [SerializeField] GameObject dog;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        dog.GetComponent<Renderer>().material = listMaterials[colorIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
